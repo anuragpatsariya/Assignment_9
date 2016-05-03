6838841a0c5af0d9caacf3d5e77b8a898dab608c
Anurag Patsariya <anurag.patsariya@outlook.com>
2016-05-02 23:03:09 -0700

    First commit

---
+++ Part 1/app.js
@@ -0,0 +1,19 @@
+/*globals ko*/
+"use strict";
+function viewPostModel() {
+    var scope = this;
+    
+    scope.comment = ko.observable("");
+    scope.comments = ko.observableArray([
+        { title : "This is first post."},
+        { title : "This is second post"},
+        { title : "This is third post"}
+    ]);
+    
+    scope.addComment = function () {
+        scope.comments.push({ title : scope.comment() });
+        scope.comment("");
+    };
+}
+
+ko.applyBindings(new viewPostModel());
